d4ff7565e8cd27674a2982c4d656bbcbc09be91c
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-08 23:14:52 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,34 @@ class App extends Component {
     super(props);
 
     this.cityList = [
-      { keys: "location1", name: "Goa", country: "India" },
-      { keys: "location2", name: "Amsterdam", country: "Netherlands" },
-      { keys: "location3", name: "New York", country: "USA" },
-      { keys: "location4", name: "Darjeeling", country: "India" },
-      { keys: "location5", name: "Tokyo", country: "Japan" },
-      { keys: "location6", name: "Lonavala", country: "India" },
-      { keys: "location7", name: "Brandenburg Gate", country: "Germany" },
-      { keys: "location8", name: "Reichstag Building", country: "Germany" },
-      { keys: "location9", name: "Museum Island", country: "Germany" },
-      { keys: "location10", name: "Munnar", country: "India" },
-      { keys: "location11", name: "Leh Ladakh", country: "India" },
-      { keys: "location12", name: "Goa", country: "India" },
-      { keys: "location13", name: "Agra", country: "India" },
-      { keys: "location14", name: "Dalhousie", country: "India" },
-      { keys: "location15", name: "Coorg", country: "India" },
-      { keys: "location16", name: "Rome", country: "Italy" },
-      { keys: "location17", name: "Milan", country: "Italy" },
-      { keys: "location18", name: "Venice", country: "Italy" },
-      { keys: "location19", name: "Varanasai", country: "India" },
-      { keys: "location20", name: "Jaipur", country: "India" },
-      { keys: "location21", name: "The Hofburg", country: "Austria" },
-      { keys: "location22", name: "Belvedere Palace", country: "Austria" },
-      { keys: "location23", name: "St. Stephen Cathedral", country: "Austria" },
-      { keys: "location24", name: "Kahna National Park", country: "India" },
-      { keys: "location25", name: "Amritsar", country: "India" },
-      { keys: "location26", name: "Mussoorie", country: "India" },
-      { keys: "location27", name: "Mount Abu", country: "India" },
-      { keys: "location28", name: "Tirupati", country: "India" }
+      { name: "Goa", country: "India" },
+      { name: "Amsterdam", country: "Netherlands" },
+      { name: "New York", country: "USA" },
+      { name: "Darjeeling", country: "India" },
+      { name: "Tokyo", country: "Japan" },
+      { name: "Lonavala", country: "India" },
+      { name: "Brandenburg Gate", country: "Germany" },
+      { name: "Reichstag Building", country: "Germany" },
+      { name: "Museum Island", country: "Germany" },
+      { name: "Munnar", country: "India" },
+      { name: "Leh Ladakh", country: "India" },
+      { name: "Goa", country: "India" },
+      { name: "Agra", country: "India" },
+      { name: "Dalhousie", country: "India" },
+      { name: "Coorg", country: "India" },
+      { name: "Rome", country: "Italy" },
+      { name: "Milan", country: "Italy" },
+      { name: "Venice", country: "Italy" },
+      { name: "Varanasai", country: "India" },
+      { name: "Jaipur", country: "India" },
+      { name: "The Hofburg", country: "Austria" },
+      { name: "Belvedere Palace", country: "Austria" },
+      { name: "St. Stephen Cathedral", country: "Austria" },
+      { name: "Kahna National Park", country: "India" },
+      { name: "Amritsar", country: "India" },
+      { name: "Mussoorie", country: "India" },
+      { name: "Mount Abu", country: "India" },
+      { name: "Tirupati", country: "India" }
     ];
   }
 
@@ -44,8 +44,8 @@ class App extends Component {
         <ol key="oListKey">
           {this.cityList
             .filter((cName) => cName.country === "India")
-            .map((e) => (
-              <li key={e.keys}> {e.name} </li>
+            .map((e, index) => (
+              <li key="location${index}"> {e.name} </li>
             ))}
         </ol>
       </div>
